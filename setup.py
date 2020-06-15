7bdfd83a419438637deaaa18d12df1069e08e2a9
Luke Roantree <lroantree@144145-ctamop-lroantree.local>
2020-06-15 13:25:33 +0100

    added new tarball dist, made tiomsaitheoir a command-line tool

--- setup.py
+++ setup.py
@@ -1,14 +1,58 @@
 from distutils.core import setup
+
+long_desc = """
+Píotón - Write Python code in Irish in IPython
+
+---
+
+Installation:
+
+pip: pip install pioton 
+
+github: clone the repo, move the contents of the pioton/ folder somewhere accessible by your PythonPATH
+
+---
+
+USAGE Transpiled:
+
+write your píotón code in one or more scripts with a '.pag' (Python as Gaeilge) extension, and run 
+
+tiomsaitheoir file.pag [file2.pag [...]]
+
+which will transpile the píotón scripts into pure Python '.py' files. Running 
+
+tiomsaitheoir -h
+
+will provide further details and examples, and running 
+
+tiomsaitheoir -s
+
+will show an overview of the syntax for píotón code 
+
+USAGE IPython:
+
+simply import píotón in any IPython environment (including Jupyter Notebooks) and start writing your code as Gaeilge.
+Call `pioton.show_syntax()` to print a full description of the new irish syntax.
+
+THIS IS FOR FUN - DO NOT USE THIS FOR ANYTHING IMPORTANT
+
+I can't stress that enough, I really didn't make sure it's robust at all, it's against Python standards, any code written in it will be completely unmaintainable, etc.
+
+---
+"""
+
 setup(
   name = 'pioton',         # How you named your package folder (MyLib)
   packages = ['pioton'],   # Chose the same as "name"
-  version = '0.2.4',      # Start with a small number and increase it with every change you make
+  scripts = ['bin/tiomsaitheoir'],
+  version = '0.3.3',      # Start with a small number and increase it with every change you make
   license='GPL',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'píotón is a tool to tweak IPython to allow coding in Irish. Not for use in any important work',   # Give a short description about your library
+  description = 'píotón language is a superset of Python allowing it to be written in Irish. Not for use in any important work',   # Give a short description about your library
+  long_description = long_desc,
   author = 'Luke Roantree',                   # Type in your name
   author_email = 'luke@roantree.com',      # Type in your E-Mail
   url = 'https://github.com/LukeRoantree4815162342/pioton',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/LukeRoantree4815162342/pioton/archive/v_024.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/LukeRoantree4815162342/pioton/archive/v_030.tar.gz',    # I explain this later on
   keywords = ['IPython', 'Gaeilge', 'Irish'],   # Keywords that define your package best
   install_requires=['ipython'],
   classifiers=[
